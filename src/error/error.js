2b82ce69e2704770f28db7b43d61fb761fe03cb1
Isaac Evavold <nomadicbit@gmail.com>
2015-09-25 19:45:44 -0500

    more descriptive error message using Ranges

--- src/error/error.js
+++ src/error/error.js
@@ -1,17 +1,66 @@
 // [MiniPy] /src/error/error.js
 
 exports.MiniPyError = (function() {
+	// given a string `ch` and a number `n`, return a new string
+	// with `ch` repeated `n` times
+	function multiplyChar(ch, n) {
+		var out = '';
+
+		while (n-- > 0) {
+			out += ch;
+		}
+
+		return out;
+	}
+
 	function MiniPyError(source, details) {
+		this.source = source;
 		this.type = details.type || 0;
 		this.message = details.message || '';
 
 		this.from = details.from || undefined;
 		this.to = details.to || undefined;
-
-		this.toString = function() {
-			return JSON.stringify(details, null, '   ');
-		};
 	}
+
+	MiniPyError.prototype.toString = function() {
+		var self = this;
+		var tab = '    ';
+
+		var message = '';
+
+		if (this.to && this.to.line > this.from.line) {
+			// TODO: currently will print every line of source code both
+			// those lines in error that those that aren't. in the future
+			// there should be a few valid lines above and below the erroneous
+			// lines to give context but certainly not the entire program
+			var lines = this.source.split('\n');
+
+			message += 'Error on lines ' + (this.from.line + 1) + ' through ' + (this.to.line + 1) + ': ' + this.message + '\n';
+
+			message += lines.map(function(line, index) {
+				if (index >= self.from.line && index <= self.to.line) {
+					return '>>' + line.replace(/\t/g, tab);
+				} else {
+					return '  ' + line.replace(/\t/g, tab);
+				}
+			}).join('\n');
+		} else {
+			var line = this.source.split('\n')[this.from.line];
+
+			var paddingWidth = line.substring(0, this.from.column).replace(/\t/g, tab).length;
+			var padding = multiplyChar('-', paddingWidth);
+
+			var underlineWidth = line.substring(0, this.to.column || line.length).replace(/\t/g, tab).length - paddingWidth;
+			var underline = multiplyChar('^', underlineWidth);
+
+			message += 'Error on line ' + (this.from.line + 1) + ': ' + this.message + '\n';
+
+			message += line.replace(/\t/g, tab) + '\n';
+			message += padding + underline + '\n';
+		}
+
+		return message;
+	};
 
 	return MiniPyError;
 }());
