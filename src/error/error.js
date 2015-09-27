6f18c41f3942aa853f325c4df2d5d23f9e3ae69f
Isaac Evavold <nomadicbit@gmail.com>
2015-09-26 22:19:01 -0500

    error messages are built by joining an array of the messages lines

--- src/error/error.js
+++ src/error/error.js
@@ -26,7 +26,7 @@ exports.MiniPyError = (function() {
 		var self = this;
 		var tab = '    ';
 
-		var message = '';
+		var message = [];
 
 		if (this.to && this.to.line > this.from.line) {
 			// TODO: currently will print every line of source code both
@@ -35,15 +35,15 @@ exports.MiniPyError = (function() {
 			// lines to give context but certainly not the entire program
 			var lines = this.source.split('\n');
 
-			message += 'Error on lines ' + (this.from.line + 1) + ' through ' + (this.to.line + 1) + ': ' + this.message + '\n';
+			message.push('Error on lines ' + (this.from.line + 1) + ' through ' + (this.to.line + 1) + ': ' + this.message);
 
-			message += lines.map(function(line, index) {
+			message.push(lines.map(function(line, index) {
 				if (index >= self.from.line && index <= self.to.line) {
-					return '>>' + line.replace(/\t/g, tab);
+					return '-' + line.replace(/\t/g, tab);
 				} else {
-					return '  ' + line.replace(/\t/g, tab);
+					return '+' + line.replace(/\t/g, tab);
 				}
-			}).join('\n');
+			}).join('\n'));
 		} else {
 			var line = this.source.split('\n')[this.from.line];
 
@@ -54,15 +54,13 @@ exports.MiniPyError = (function() {
 			var underlineWidth = this.to.column - this.from.column;
 			var underline = multiplyChar('^', underlineWidth);
 
-			console.log('underlineWidth', this.to.column - this.from.column);
+			message.push('Error on line ' + (this.from.line + 1) + ': ' + this.message);
 
-			message += 'Error on line ' + (this.from.line + 1) + ': ' + this.message + '\n';
-
-			message += line.replace(/\t/g, tab) + '\n';
-			message += padding + underline + '\n';
+			message.push(line.replace(/\t/g, tab));
+			message.push(padding + underline);
 		}
 
-		return message;
+		return message.join('\n') + '\n';
 	};
 
 	return MiniPyError;
