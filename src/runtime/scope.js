9ed35845ef92910b3071cf5eccb18f353a0547ef
Isaac Evavold <nomadicbit@gmail.com>
2015-10-02 01:08:56 -0500

    overhaul internal interpreter and execution flow-control system
    + also adds support for executing functions

--- src/runtime/scope.js
+++ src/runtime/scope.js
@@ -17,51 +17,100 @@ exports.Scope = (function() {
 				}
 
 				return simplification;
+			case 'Function':
+				return 'def (' + value.args.map(function(arg) {
+					return arg.value;
+				}).join(', ') + ')';
 			default:
 				return undefined;
 		}
 	}
 
 	function Scope(parent) {
-		this.parent = parent || null;
-		this.local = {};
+		this.parent = parent;
+
+		if (this.parent === null) {
+			this.globals = {};
+		} else {
+			this.locals = {};
+		}
 	}
 
-	Scope.prototype.isLocal = function(name) {
-		if (this.local.hasOwnProperty(name)) {
-			return true;
+	Scope.prototype.isGlobalScope = function() {
+		return (this.parent === null);
+	};
+
+	Scope.prototype.owns = function(name) {
+		// return true if variable with `name` exists in this scope
+		if (this.isGlobalScope()) {
+			return this.globals.hasOwnProperty(name);
 		} else {
-			return false;
+			return this.locals.hasOwnProperty(name);
 		}
+	};
+
+	Scope.prototype.exists = function(name) {
+		// return true if variable with `name` has
+		// been created in any available scope
+		return this.owns(name) || (!this.isGlobalScope() && this.parent.owns(name));
 	};
 
 	Scope.prototype.get = function(node) {
 		var name = node.value;
 
-		if (this.isLocal(name)) {
-			return this.local[name];
+		if (this.owns(name)) {
+			if (this.isGlobalScope()) {
+				return this.globals[name];
+			} else {
+				return this.locals[name];
+			}
 		} else {
-			if (this.parent !== null && typeof this.parent.get === 'function') {
+			// this scope doesn't own the variable, check with the parent scope
+			// or throw an error if it doesn't exist
+			if (this.parent instanceof Scope) {
 				return this.parent.get(name);
 			} else {
-				throw node.range.error({
+				throw node.error({
 					type: ErrorType.UNDEFINED_VARIABLE,
 					message: 'No variable with identifier "' + name + '"',
-					range: node.range,
 				});
 			}
 		}
 	};
 
-	Scope.prototype.set = function(node, value) {
+	Scope.prototype.set = function(node, value, forceLocal) {
 		var name = node.value || node.toString();
 
-		if (this.isLocal(name)) {
-			this.local[name] = value;
-		} else if (this.parent !== null) {
-			this.parent.set(name, value);
+		if (forceLocal === true) {
+			// variable is forced to be created locally (usually
+			// for function arguments)
+			if (this.isGlobalScope()) {
+				this.globals[name] = value;
+			} else {
+				this.locals[name] = value;
+			}
+		} else if (this.exists(name)) {
+			// variable already created, modify its value where it resides
+			if (this.owns(name)) {
+				if (this.isGlobalScope()) {
+					this.globals[name] = value;
+				} else {
+					this.locals[name] = value;
+				}
+			} else {
+				if (this.isGlobalScope()) {
+					this.globals[name] = value;
+				} else {
+					this.parent.set(node, value);
+				}
+			}
 		} else {
-			this.local[name] = value;
+			// variable doesn't exist anywhere already, create it locally
+			if (this.isGlobalScope()) {
+				this.globals[name] = value;
+			} else {
+				this.locals[name] = value;
+			}
 		}
 	};
 
@@ -84,7 +133,7 @@ exports.Scope = (function() {
 		if (this.parent === null) {
 			return scope;
 		} else {
-			return this.parent(scope);
+			return this.parent.toJSON(scope);
 		}
 	};
 
